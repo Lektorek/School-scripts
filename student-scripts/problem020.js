b668be049f4030cc8cef48ffed6d595af5564672
Adi <adrian.skrzydo@gmail.com>
2020-10-29 14:47:02 +0100

    Życie obiektów

---
+++ student-scripts/problem020.js
@@ -0,0 +1,11 @@
+class Square {
+    constructor(a){
+        this.a = a;
+    }
+    getArea(){
+        return this.a*this.a;
+    }
+    getPerimeter(){
+        return this.a+this.a+this.a+this.a;
+    }
+}
